37d3ae6f15e0136f81072d5666ccb10e567d28f5
Bluegent <bluegentleman@live.com>
2020-12-04 11:35:46 +0200

    Added simple library for wrapping commproto functionality for arduino

---
+++ libraries/DeviceWrapper/DeviceWrapper.h
@@ -0,0 +1,33 @@
+#ifndef DEVICE_WRAPPER_H
+#define DEVICE_WRAPPER_H
+
+#include <commproto/variable/Context.h>
+#include <commproto/parser/MessageBuilder.h>
+#include <SocketImpl.h>
+#include "SDKWrapper.h"
+
+
+enum class DeviceState
+{
+	Searching,
+	Initializing,
+	Connected,
+};
+
+
+using namespace commproto;
+class DeviceWrapper {
+public:
+	DeviceWrapper(SDKWrapper & device,const char * host, uint32_t port);
+	void setup();
+	DeviceState loop();
+private:
+	commproto::sockets::SocketHandle client;
+	variable::ContextHandle ctx;
+	parser::MessageBuilderHandle builder;
+	std::string host;
+	uint32_t port;
+	SDKWrapper& device;
+	DeviceState state;
+};
+#endif 
